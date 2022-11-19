fd00433d216420ab5095394d91092039602382b3
lul <83666061+lul0101@users.noreply.github.com>
2022-11-19 11:30:12 -0300

    Centralização arrumada e começo das paginas novas

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,12 @@
 <?php
-	session_start();
-	if (!isset($_SESSION["logado"])||$_SESSION["logado"]!= TRUE) {
-		header("Location: login.php");
-	}
+session_start();
+if (!isset($_SESSION["logado"]) || $_SESSION["logado"] != TRUE) {
+	header("Location: login.php");
+}
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="UTF-8">
 	<title>Adibas</title>
@@ -16,44 +17,53 @@
 	<link rel="stylesheet" href="../style/styleadm.css">
 	<script src="bootstrap\node_modules\bootstrap\dist\js\bootstrap.bundle.js"></script>
 
-	</head>
-<body class="center bg-secondary">
-<div class="container">
+</head>
+
+<body class="center-index bg-secondary">
+	<div class="container">
 		<div class="row align-items-center rounded-3 border shadow-lg bg-white" style="padding: 2rem; justify-content: center;">
 			<div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
 				<main class="form-signin w-100 m-auto" style="display:flex; flex-direction:column; align-items:center; gap:2rem; text-align:center;">
-				<img src="../imagens/Adibas.png" alt="Logo" width="180" height="128">
-				<div style="display:flex; flex-direction:column; gap:2rem; ">
-					<?php
-						echo "<h1>Seja bem-vindo, ".$_SESSION["nome"]."!</h1>";
-						if (($_SESSION["funcao"] === "gerente")) {?>
+					<img src="../imagens/Adibas.png" alt="Logo" width="180" height="128">
+					<div style="display:flex; flex-direction:column; gap:2rem; ">
+						<?php
+						echo "<h1>Seja bem-vindo, " . $_SESSION["nome"] . "!</h1>";
+						if (($_SESSION["funcao"] === "gerente")) { ?>
 							<form action="listarProduto.php">
-							<button type="submit" class="w-100 btn btn-lg btn-secondary" type="submit">Produtos</button>
-							</form>
-							
-							<form action="relatorioVendas.php">
-							<button type="submit" class="w-100 btn btn-lg btn-secondary" type="submit">Relatório de Vendas</button>
+								<button type="submit" class="w-100 btn btn-lg btn-secondary" type="submit">Produtos</button>
 							</form>
 
 							<form action="listarFuncionario.php">
-							<button type="submit" class="w-100 btn btn-lg btn-secondary" type="submit">Funcionários</button>
-							</form> 
-							
-							<?php } else{ ?>
-								<form action="listarFuncionario.php">
-									<button type="submit" class="w-100 btn btn-lg btn-secondary">Alterar Dados</button>
-								</form> 					
-							<?php }?> 
-						
+								<button type="submit" class="w-100 btn btn-lg btn-secondary" type="submit">Funcionários</button>
+							</form>
+
+							<form action="relatorioVendas.php">
+								<button type="submit" class="w-100 btn btn-lg btn-secondary" type="submit">Relatório de Vendas</button>
+							</form>
+
+							<form action="financeiro.html">
+								<button type="submit" class="w-100 btn btn-lg btn-secondary" type="submit">Financeiro</button>
+							</form>
+							<form action="expedi.html">
+								<button type="submit" class="w-100 btn btn-lg btn-secondary" type="submit">Expedição</button>
+							</form>
+
+						<?php } else { ?>
+							<form action="listarFuncionario.php">
+								<button type="submit" class="w-100 btn btn-lg btn-secondary">Alterar Dados</button>
+							</form>
+						<?php } ?>
+
 						<form action="sair.php">
-						<button type="submit" class="w-100 btn btn-lg btn-secondary" type="submit">Sair</button>
+							<button type="submit" class="w-100 btn btn-lg btn-secondary" type="submit">Sair</button>
 						</form>
-						</div>
-                  </main>
+					</div>
+				</main>
 			</div>
 		</div>
 	</div>
-		
+
 
 </body>
-</html> 
+
+</html>
